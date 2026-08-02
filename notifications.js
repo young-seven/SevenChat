d2e031e18c34f126a6359ae75847270089c9a192
young-seven <kingsleyasante1234@icloud.com>
2026-08-02 08:53:09 +0000

    Fix import statement formatting
    Fixed import statement formatting in notifications.js.

--- notifications.js
+++ notifications.js
@@ -1,11 +1,11 @@
-import { app } from "./firebase.js";
-
+import { app } from"./firebase.js";
 import {
   getMessaging,
   isSupported,
   getToken,
   onMessage
 } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-messaging.js";
+alert("NOTIFICATION.JS LOADED")
 
 const VAPID_KEY =
   "BJoNRtYK6d5sXO3GkuPoKSBrsTvqaWAS1aCCDMCqnMSGVdTfzASPX8zZKGWf3nFeCzNp8V-RY2_-DlWamnSZwR8";
